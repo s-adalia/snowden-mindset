452ce0679ff80926b1ce05804008eed36ec31edb
s-adalia-jr <samantha_adalia@edu.aua.am>
2021-05-07 17:52:17 +0400

    Login-logout progress.
    Added stuff to server/js and login

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser')
+
+// Google Auth
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID = '476306934446-6f3755lqe4m9jimq5vqctu1b8hefhhkp.apps.googleusercontent.com'
+const client = new OAuth2Client(CLIENT_ID);
+
 
 const PORT = process.env.PORT || 5001;
 
@@ -7,6 +14,43 @@ app.use(express.static('public'));
 
 app.get("/", (req, res) => {
 	res.render("index");
+})
+
+app.get("/login", (req, res) => {
+	res.render("login");
+})
+
+app.post('/login', (req,res)=>{
+    let token = req.body.token;
+
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        const userid = payload['sub'];
+      }
+      verify()
+      .then(()=>{
+          res.cookie('session-token', token);
+          res.send('success')
+      })
+      .catch(console.error);
+
+})
+
+app.get("/login_index", (req, res) => {
+	res.render("login_index");
+})
+
+app.get('/profile',  (req, res)=>{
+    let user = req.user;
+    res.render('profile', {user});
+})
+
+app.get('/protectedRoute',  (req,res)=>{
+    res.send('This route is protected')
 })
 
 app.get("/survey_books", (req, res) => {
@@ -21,8 +65,15 @@ app.get("/admin", (req, res) => {
 	res.render("admin");
 })
 
+app.get("/edit_books", (req, res) => {
+	res.render("edit_books");
+})
+
 app.listen(PORT, () => {
 	console.log("Server has started on: " + PORT);
     })
 
 app.set("view engine", "ejs");
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
